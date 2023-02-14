d012b0f9c6e7907c822c5c5f76be4e585d28c992
Lelepons25 <lyllkristine.cabelin@cit.edu>
2023-02-14 23:31:45 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DFWSv1._1
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FruitsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FruitsButton
+            // 
+            this.FruitsButton.Location = new System.Drawing.Point(56, 172);
+            this.FruitsButton.Name = "FruitsButton";
+            this.FruitsButton.Size = new System.Drawing.Size(107, 37);
+            this.FruitsButton.TabIndex = 0;
+            this.FruitsButton.Text = "Fruits";
+            this.FruitsButton.UseVisualStyleBackColor = true;
+            this.FruitsButton.Click += new System.EventHandler(this.FruitsButton_Click);
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 473);
+            this.Controls.Add(this.FruitsButton);
+            this.Name = "Homepage";
+            this.Text = "DFWS";
+            this.Load += new System.EventHandler(this.Homepage_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FruitsButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DFWSv1._1
+{
+    public partial class Homepage : Form
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void Homepage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FruitsButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Fruits fruits = new Fruits();
+            fruits.Show();
+        }
+    }
+}
